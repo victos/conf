489ba0928948156fdffa571d2dbfcd68c698b4ef
milyuu <miller.yu@leandev.se>
2019-12-17 09:47:10 +0800

    update config

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ ln -s "$PROJECT_DIR/conky" "$CONFIG_DIR/conky"
 ln -s "$PROJECT_DIR/nvim" "$CONFIG_DIR/nvim"
 ln -s "$PROJECT_DIR/tmux" "$CONFIG_DIR/tmux"
 ln -s "$PROJECT_DIR/polybar" "$CONFIG_DIR/polybar"
+ln -s "$PROJECT_DIR/autorandr" "$CONFIG_DIR/../.autorandr"
 
 mkdir -p .local/share/fonts
 cp "$PROJECT_DIR/conky/Conky\ Icons\ by\ Carelli.ttf" "$HOME/.local/share/fonts/"
