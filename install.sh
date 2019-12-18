039b99110c13edbfbca20579c0cf6b0a7519866e
milyuu <miller.yu@leandev.se>
2019-12-18 11:07:24 +0800

    update fish

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ ln -s "$PROJECT_DIR/nvim" "$CONFIG_DIR/nvim"
 ln -s "$PROJECT_DIR/tmux" "$CONFIG_DIR/tmux"
 ln -s "$PROJECT_DIR/polybar" "$CONFIG_DIR/polybar"
 ln -s "$PROJECT_DIR/autorandr" "$CONFIG_DIR/../.autorandr"
+ln -s "$PROJECT_DIR/fish" "$CONFIG_DIR/fish"
 
 mkdir -p .local/share/fonts
 cp "$PROJECT_DIR/conky/Conky\ Icons\ by\ Carelli.ttf" "$HOME/.local/share/fonts/"
