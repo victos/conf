a4553ecfd1530023509daab64fe42641d71e781e
milyuu <miller.yu@leandev.se>
2019-12-20 09:05:01 +0800

    update config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,8 @@
 set theme_color_scheme solarized
 set -x GOPATH ~/go
 set -x PATH $PATH $GOPATH/bin
+
+if test -n "$DESKTOP_SESSION"
+    set (gnome-keyring-daemon --start | string split "=")
+end
+set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/keyring/ssh"
